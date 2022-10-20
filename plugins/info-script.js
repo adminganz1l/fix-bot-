48a2a63713bb9395351682dda0c0bdc27993d781
adminganz1l <115880190+adminganz1l@users.noreply.github.com>
2022-10-20 19:25:12 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
@@ -15,10 +15,10 @@ wa.me/6282127487538`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG", 
+    title: '  https://chat.whatsapp.com/D71CKTjDBgkBYmaNZuUmcG ',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
